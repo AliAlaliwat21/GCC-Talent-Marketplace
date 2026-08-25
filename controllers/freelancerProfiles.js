f86d617ef8d1eb9039f2a1a84074668b35d555a1
Jassiiimm <owainatijassim@gmail.com>
2026-08-25 18:21:40 +0300

    Added update freelancer profile

--- controllers/freelancerProfiles.js
+++ controllers/freelancerProfiles.js
@@ -33,14 +33,43 @@ const create = async (req, res) => {
 
         res.status(201).json(profile)
     } catch (error) {
-        res.status(400).json({
-            message: error.message
-        })
+        res.status(400).json({message: error.message})
     }
 }
+
+const update = async (req, res) => {
+    try {
+        const profile = await FreelancerProfile.findById(req.params.id)
+        
+        if (!profile) {
+            return res.status(404).json({message: 'Freelancer profile not found'})
+        }
+        
+        if (profile.user.toString() !== req.user._id.toString()) { return res.status(401).json({ message: 'You cannot update this profile'})
+        }
+    
+    const updatedProfile = await FreelancerProfile.findByIdAndUpdate(
+        req.params.id,
+        {
+            headline: req.body.headline,
+            bio: req.body.bio,
+            skills: req.body.skills,
+            hourlyRate: req.body.hourlyRate,
+            languages: req.body.languages,
+            availability: req.body.availability
+        }, { new: true }
+    )
+    
+    res.status(200).json(updatedProfile)
+} catch (error) {
+    res.status(500).json({message: error.message})
+    }
+}
+
 
 module.exports = {
     index,
     show,
-    create
+    create,
+    update,
 }
