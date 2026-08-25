7cdbdd5962ddeb0d7dbfe320539831f082077644
HALI12_hat <hussainaliabdulrasool004@gmail.com>
2026-08-26 01:11:09 +0300

    added the refresh function which retrieves the refreshtoken from httponly cookie after the accesstoken expires, verifies JWT, checks that current user id is the same as in the token payload, compares raw token with the hashed in monogdb, then generates a new accesstoken and rotates the refreshtoken by generating a new one as well replacing the hashed old one in mongodb and replaces the old cookie in the browser

--- controllers/auth.js
+++ controllers/auth.js
@@ -88,6 +88,7 @@ const signIn = async (req, res) => {
         }
 
         const payload = { username: userInDatabase.username, _id: userInDatabase._id, email: userInDatabase.email, role: userInDatabase.role}
+
         const accessToken = jwt.sign({ payload }, process.env.JWT_SECRET, {expiresIn: '30m'})
 
         const refreshToken = jwt.sign({payload}, process.env.REFRESH_TOKEN_SECRET, {expiresIn: '7d'})
@@ -111,6 +112,55 @@ const signIn = async (req, res) => {
     }
 }
 
+const refresh = async (req, res)=>{
+    try {
+        const refreshToken = req.cookies.refreshToken
+
+        if (!refreshToken) return res.status(401).json({message: 'Refresh Token Required.'})
+
+        const decoded = jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET)
+
+        const user = await User.findById(decoded.payload._id)
+
+        if (!user) return res.status(404).json({
+            message: 'User not found.'
+        })
+
+        const validRefreshToken = bcrypt.compareSync(refreshToken, user.refreshTokenHash)
+
+        if (!user.refreshTokenHash) return res.status(401).json({
+        message: 'Invalid refresh token.'
+        })
+
+        if (!validRefreshToken) return res.status(401).json({
+            message: 'Invalid Refresh Token.'
+        })
+
+        const payload = { username: user.username, _id: user._id, email: user.email, role: user.role }
+
+        const newAccessToken = jwt.sign({payload}, process.env.JWT_SECRET, {expiresIn: '30m'})
+
+        const newRefreshToken = jwt.sign({payload}, process.env.REFRESH_TOKEN_SECRET, {expiresIn: '7d'})
+
+        const newRefreshTokenHash = bcrypt.hashSync(newRefreshToken, 10)
+
+        user.refreshTokenHash = newRefreshTokenHash
+        await user.save()
+
+        res.cookie('refreshToken', newRefreshToken, {
+            httpOnly: true,
+            sameSite: 'lax',
+            maxAge:  7 * 24 * 60 * 1000
+        })
+
+        res.status(200).json({
+            accessToken: newAccessToken
+        })
+    } catch (err) {
+        res.status(500).json({ err: err.message })
+    }
+}
+
 
 
 module.exports = {
@@ -118,6 +168,7 @@ module.exports = {
     // verifyToken,
     signUp,
     signIn,
+    refresh
 }
 
 
