11be0da9c105c240d864514f4ef4bac9064caff3
Jassiiimm <owainatijassim@gmail.com>
2026-08-25 18:41:30 +0300

    Added the ability to update freelancer portfolio items

--- controllers/freelancerProfiles.js
+++ controllers/freelancerProfiles.js
@@ -93,6 +93,39 @@ const createPortfolioItem = async (req, res) => {
     }
 }
 
+const updatePortfolioItem = async (req, res) => {
+    try {
+        const profile = await FreelancerProfile.findById(req.params.id)
+        
+        if (!profile) {
+            return res.status(404).json({message: 'Freelancer profile not found'})
+        }
+
+        if (profile.user.toString() !== req.user._id.toString()) {
+            return res.status(401).json({message: 'You cannot update this profile'})
+        }
+        
+        const portfolioItem = profile.portfolio.find((item) => {
+        return item._id.toString() === req.params.portfolioId
+    })
+
+        if (!portfolioItem) {
+            return res.status(404).json({message: 'Portfolio item not found'})
+        }
+        portfolioItem.title = req.body.title
+        portfolioItem.description = req.body.description
+        portfolioItem.imageUrl = req.body.imageUrl
+        portfolioItem.link = req.body.link
+
+        await profile.save()
+        
+        res.status(200).json(profile)
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+}
+
+
 
 
 
@@ -101,4 +134,6 @@ module.exports = {
     show,
     create,
     update,
+    createPortfolioItem,
+    updatePortfolioItem
 }
