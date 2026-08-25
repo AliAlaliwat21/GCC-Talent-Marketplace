695008e7e75aed52f81b799c5ad0fd1618ebcfe2
Jassiiimm <owainatijassim@gmail.com>
2026-08-25 18:11:34 +0300

    Added freelancer profile show

--- controllers/freelancerProfiles.js
+++ controllers/freelancerProfiles.js
@@ -5,6 +5,21 @@ const index = async (req, res) => {
     res.json(profiles)
 }
 
+const show = async (req, res) => {
+    try {
+        const profile = await FreelancerProfile.findById(req.params.id)
+        
+        if (!profile) {
+            return res.status(404).json({ message: 'Freelancer profile not found'})
+        }
+        
+        res.status(200).json(profile)
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+}
+
 module.exports = {
-    index
+    index,
+    show
 }
