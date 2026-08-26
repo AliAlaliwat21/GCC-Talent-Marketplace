d707e450627f86586f6c5e9214e6fc2421b58c39
Jassiiimm <owainatijassim@gmail.com>
2026-08-27 01:53:22 +0300

    Added category index function

---
+++ controllers/categories.js
@@ -0,0 +1,17 @@
+const Category = require('../models/category')
+
+const index = async (req, res) => {
+    try {
+        const categories = await Category.find()
+
+        res.status(200).json(categories)
+    } catch (error) {
+        res.status(500).json({
+            message: error.message
+        })
+    }
+}
+
+module.exports = {
+    index
+}
