8ea30fceee98f671a9b718828c58196f1156a3f9
Jassiiimm <owainatijassim@gmail.com>
2026-08-27 02:35:16 +0300

    Added skill update function

--- controllers/skills.js
+++ controllers/skills.js
@@ -30,7 +30,36 @@ const create = async (req, res) => {
     }
 }
 
+const update = async (req, res) => {
+    try {
+        if (req.user.role !== 'admin') {
+            return res.status(403).json({message: "Only admins can update skills"})
+        }
+        const updatedSkill = await Skill.findByIdAndUpdate(
+            req.params.id,
+            {
+                name: req.body.name,
+                slug: req.body.slug,
+                category: req.body.category
+            },
+            {
+                new: true,
+                runValidators: true
+            }
+        )
+
+        if (!updatedSkill) {
+            return res.status(404).json({message: "Skill not found"})
+        }
+
+        res.status(200).json(updatedSkill)
+    } catch (error) {
+        res.status(400).json({message: error.message})
+    }
+}
+
 module.exports = {
     index,
     create,
+    update,
 }
