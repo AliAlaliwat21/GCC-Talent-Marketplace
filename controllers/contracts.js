8081f9afdf6c14a9c0ba2aa033a438571f2cafb5
Jassiiimm <owainatijassim@gmail.com>
2026-08-27 00:15:29 +0300

    Added milestone update function

--- controllers/contracts.js
+++ controllers/contracts.js
@@ -66,8 +66,61 @@ const addMilestone = async (req, res) => {
     }
 }
 
+const updateMilestone = async (req, res) => {
+    try {
+        const contract = await Contract.findById(req.params.id)
+        if (!contract) {
+            return res.status(404).json({message: "Contract not found"})
+        }
+        if (contract.client.toString() !== req.user._id.toString()) {
+            return res.status(403).json({message: "You cannot update this milestone"})
+        }
+        
+        const milestone = contract.milestones.id(req.params.mid)
+        
+        if (!milestone) {
+            return res.status(404).json({message: "Milestone not found"})
+        }
+
+        if (milestone.status !== 'pending') {
+            return res.status(400).json({
+                message: "Only unfunded milestones can be updated"
+            })
+        }
+
+        if (req.body.title !== undefined) {
+            milestone.title = req.body.title
+        }
+
+        if (req.body.description !== undefined) {
+            milestone.description = req.body.description
+        }
+
+        if (req.body.amount !== undefined) {
+            milestone.amount = req.body.amount
+        }
+
+        if (req.body.dueDate !== undefined) {
+            milestone.dueDate = req.body.dueDate
+        }
+
+        contract.activity.push({
+            type: 'milestone_updated',
+            by: req.user._id,
+            message: `Milestone updated: ${milestone.title}`
+        })
+
+        await contract.save()
+        
+        res.status(200).json(contract)
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+}
+
 module.exports = {
     index,
     show,
     addMilestone,
+     updateMilestone,
 }
