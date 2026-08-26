87930b11af2aad0dd815998ad9782b59e1df4a6d
Jassiiimm <owainatijassim@gmail.com>
2026-08-27 00:00:38 +0300

    Added contract milestone function

--- controllers/contracts.js
+++ controllers/contracts.js
@@ -4,10 +4,10 @@ const index = async (req, res) => {
     try {
         let contracts
         
-        if (req.user.role === 'client') {
+        if (req.user.role === "client") {
             contracts = await Contract.find({client: req.user._id})
         }
-        if (req.user.role === 'freelancer') {
+        if (req.user.role === "freelancer") {
             contracts = await Contract.find({freelancer: req.user._id})
         }
         
@@ -22,14 +22,45 @@ const show = async (req, res) => {
         const contract = await Contract.findById(req.params.id)
         
         if (!contract) {
-            return res.status(404).json({message: 'Contract not found'})
+            return res.status(404).json({message: "Contract not found"})
         }
         if (contract.client.toString() !== req.user._id.toString() && contract.freelancer.toString() !== req.user._id.toString()) {
-            return res.status(403).json({
-                message: 'You cannot view this contract'
-            })
+            return res.status(403).json({message: "You cannot view this contract"})
         }
         res.status(200).json(contract)
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+}
+
+const addMilestone = async (req, res) => {
+    try {
+        const contract = await Contract.findById(req.params.id)
+        if (!contract) {
+            return res.status(404).json({message: "Contract not found"})
+        }
+        if (contract.client.toString() !== req.user._id.toString()) {
+            return res.status(403).json({message: "You cannot add a milestone to this contract"})
+        }
+        if (contract.status !== 'active') {
+            return res.status(400).json({message: "Milestones can only be added to active contracts"})
+        }
+        contract.milestones.push({
+            title: req.body.title,
+            description: req.body.description,
+            amount: req.body.amount,
+            dueDate: req.body.dueDate
+        })
+
+        contract.activity.push({
+            type: 'milestone_added',
+            by: req.user._id,
+            message: `Milestone added: ${req.body.title}`
+        })
+
+        await contract.save()
+        
+        res.status(201).json(contract)
     } catch (error) {
         res.status(500).json({message: error.message})
     }
@@ -38,4 +69,5 @@ const show = async (req, res) => {
 module.exports = {
     index,
     show,
+    addMilestone,
 }
