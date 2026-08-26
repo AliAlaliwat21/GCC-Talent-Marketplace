df19700b1b8757d37b0e3964251818828309393b
Jassiiimm <owainatijassim@gmail.com>
2026-08-26 12:54:07 +0300

    Added category model and fields

---
+++ models/category.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+
+const categorySchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true,
+    },
+    slug: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        unique: true,
+    },
+    icon: {
+        type: String,
+    },
+    isFeatured: {
+        type: Boolean,
+        default: false,
+    },
+}, {timestamps: true})
+
+const Category = mongoose.model('Category', categorySchema)
+
+module.exports = Category
