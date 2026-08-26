bebc83f49871cd14bd52c8ee1bbeab63435dce93
Jassiiimm <owainatijassim@gmail.com>
2026-08-27 01:58:44 +0300

    Added category update function

--- controllers/categories.js
+++ controllers/categories.js
@@ -15,7 +15,7 @@ const index = async (req, res) => {
 const create = async (req, res) => {
     try {
         if (req.user.role !== 'admin') {
-            return res.status(403).json({ message:"Only admins can create categories"})
+            return res.status(403).json({ message: "Only admins can create categories"})
         }
         const category = await Category.create({
             name: req.body.name,
@@ -30,7 +30,34 @@ const create = async (req, res) => {
     }
 }
 
+const update = async (req, res) => {
+    try {
+        if (req.user.role !== 'admin') {
+            return res.status(403).json({message: "Only admins can update categories"})
+        }
+        const updatedCategory = await Category.findByIdAndUpdate(req.params.id,
+            {
+                name: req.body.name,
+                slug: req.body.slug,
+                icon: req.body.icon,
+                isFeatured: req.body.isFeatured
+            },
+            {
+                new: true,
+                runValidators: true
+            }
+        )
+        if (!updatedCategory) {
+            return res.status(404).json({message: "Category not found"})
+        }
+        res.status(200).json(updatedCategory)
+    } catch (error) {
+        res.status(400).json({message: error.message})
+    }
+}
+
 module.exports = {
     index,
     create,
+    update,
 }
