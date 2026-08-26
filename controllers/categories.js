c1f878eaf381ce52a3aedd6e0bafa32ffba35761
Jassiiimm <owainatijassim@gmail.com>
2026-08-27 01:55:44 +0300

    Added category create function

--- controllers/categories.js
+++ controllers/categories.js
@@ -12,6 +12,25 @@ const index = async (req, res) => {
     }
 }
 
+const create = async (req, res) => {
+    try {
+        if (req.user.role !== 'admin') {
+            return res.status(403).json({ message:"Only admins can create categories"})
+        }
+        const category = await Category.create({
+            name: req.body.name,
+            slug: req.body.slug,
+            icon: req.body.icon,
+            isFeatured: req.body.isFeatured
+        })
+        
+        res.status(201).json(category)
+    } catch (error) {
+        res.status(400).json({message: error.message})
+    }
+}
+
 module.exports = {
-    index
+    index,
+    create,
 }
