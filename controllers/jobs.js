1e232e68a92e36c3bf56a950f9cd35d6ab4dcb8e
HALI12_hat <hussainaliabdulrasool004@gmail.com>
2026-08-26 18:56:27 +0300

    created the updateJob function

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -36,7 +36,7 @@ const showJob = async (req, res)=>{
         })
 
         res.status(200).json(singleJob)
-        
+
     } catch (error) {
         res.status(500).json({message: error.message})
     }
@@ -53,8 +53,48 @@ const allJobs = async (req, res) =>{
     }
 }
 
+const updateJob = async (req, res)=>{
+    try {
+        const findJob = await Job.findById(req.params.jobId)
+
+        if (!findJob) return res.status(404).json({
+            message: 'Job not found!'
+        })
+
+        if (!findJob.client.equals(req.user._id)) return res.status(403).json({
+            message: 'You are not authorized to take such action!'
+        })
+
+        if (findJob.status !== 'open') return res.status(400).json({
+            message: 'Job cannot be edited!'
+        })
+
+        const jobData = {
+            title: req.body.title,
+            description: req.body.description,
+            category: req.body.category,
+            skills: req.body.skills,
+            budgetType: req.body.budgetType,
+            budgetMin: req.body.budgetMin,
+            budgetMax: req.body.budgetMax,
+            experienceLevel: req.body.experienceLevel,
+            duration: req.body.duration,
+            deadline: req.body.deadline,
+            attachments: req.body.attachments
+        }
+
+        const updatedJob = await Job.findByIdAndUpdate(req.params.jobId, jobData, {new: true, runValidators: true})
+
+        res.status(200).json(updatedJob)
+
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+}
+
 module.exports = {
     create,
     showJob,
-    allJobs
+    allJobs,
+    updateJob
 }
