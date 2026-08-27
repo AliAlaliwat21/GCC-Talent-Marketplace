c38c01d7a25f31e9a1dc5815184750b11715fbe9
Jassiiimm <owainatijassim@gmail.com>
2026-08-27 11:14:11 +0300

    Added admin updateStatus function to suspend and reactivate users

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,11 +1,11 @@
 const User = require('../models/user')
+const { update } = require('./skills')
 
 const index = async (req, res) => {
     try {
         if (req.user.role !== "admin") {
             return res.status(403).json({message: "Only admins can view users"})
         }
-        
         const filter = {}
         if (req.query.username) {
             filter.username = req.query.username
@@ -28,6 +28,34 @@ const index = async (req, res) => {
     }
 }
 
+const updateStatus = async (req, res) => {
+    try {
+        if (req.user.role !== "admin") {
+            return res.status(403).json({message: "Only admins can change account status"})
+        }
+        if (req.body.status !== "active" && req.body.status !== "suspended") {
+            return res.status(400).json({
+                message: "Status must be active or suspended"
+            })
+        }
+        const updatedUser = await User.findByIdAndUpdate(req.params.id,
+            {
+                status: req.body.status
+            },
+            {
+                new: true,
+                runValidators: true
+            }
+        )
+        if (!updatedUser) {
+            return res.status(404).json({ message: "User not found"})
+        }
+        res.status(200).json(updatedUser)
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+}
 module.exports = {
-    index
+    index,
+    updateStatus,
 }
