ea395849a3319a04fefada0edc67eaacf580fa3b
Jassiiimm <owainatijassim@gmail.com>
2026-08-27 12:46:48 +0300

    Added account status check to token middleware

--- middleware/verify-token.js
+++ middleware/verify-token.js
@@ -1,14 +1,33 @@
 const jwt = require('jsonwebtoken')
+const User = require('../models/user')
 
-const verifyToken = (req, res, next) => {
+const verifyToken = async (req, res, next) => {
     try {
+        if (!req.headers.authorization) {
+            return res.status(401).json({message: "Token required"})
+        }
+        
         const token = req.headers.authorization.split(' ')[1]
         const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        const user = await User.findById(decoded.payload._id)
 
-        req.user = decoded.payload
+        if (!user) {
+            return res.status(401).json({message: "User not found"})
+        }
+        if (user.status === "suspended") {
+            return res.status(403).json({message: "Your account is suspended"})
+        }
+        req.user = {
+            username: user.username,
+            _id: user._id,
+            email: user.email,
+            role: user.role,
+            status: user.status
+        }
+        
         next()
-    } catch (err) {
-        res.status(401).json({ err: 'Invalid token' })
+    } catch (error) {
+        res.status(401).json({message: "Invalid token"})
     }
 }
 
