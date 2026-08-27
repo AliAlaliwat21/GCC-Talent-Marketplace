f29f306b3c61cd6417c8e8e8254a8701c6f03f0c
Ali <Alialaliwat21@gmail.com>
2026-08-27 10:18:07 +0300

    added create function fully

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -23,6 +23,49 @@ const create = async(req,res)=>{
                 message: 'Contract not found'
             })
         }
+
+        if (
+            contract.client.toString() !== req.user._id.toString() &&
+            contract.freelancer.toString() !== req.user._id.toString()
+        ){
+            return res.status(403).json({
+                message: 'You cannot review this contract'
+            })
+        }
+
+        if(contract.status !== 'completed'){
+            return res.status(422).json({
+                message: 'You can only review a completed contract'
+            })
+        }
+
+        const existingReview = await Review.findOne({
+            contract:  contract._id,
+            reviewer: req.user._id
+    })
+    if(existingReview){
+        return res.status(409).json({
+            message:'You have already reviewed this contract'
+        })
+    }
+
+    let reviewee
+
+    if(contract.client.toString() === req.user._id.toString()){
+        reviewee = contract.freelancer
+    } else{
+        reviewee = contract.client
+    }
+
+    const review = await Review.create({
+            contract: contract._id,
+            reviewer: req.user._id,
+            reviewee: reviewee,
+            rating: req.body.rating,
+            comment: req.body.comment
+    })
+
+    res.status(201).json(review)
     }catch (err) {
         res.status(500).json({
             message:err.message
