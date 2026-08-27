0c21ba62f54ceef9783ff5135c9d521fd1fe4766
Ali <Alialaliwat21@gmail.com>
2026-08-27 12:29:59 +0300

    added config folder with cloudinary fetching api key from env, added middleware that filters the allowed type of files and size limits

---
+++ middleware/upload.js
@@ -0,0 +1,30 @@
+const multer = require('multer')
+
+const storage = multer.memoryStorage()
+
+const fileFilter = (req,file,cb)=>{
+    const allowedTypes = [
+        'image/jpeg',
+        'image/png',
+        'image/webp',
+        'image/gif',
+        'application/pdf',
+        'application/msword',
+        'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+    ]
+
+    if(!allowedTypes.includes(file.mimetype)){
+        return cb(new Error('File type is not allowed'))
+    }
+    cb(null,true)
+}
+
+const uploaf = multer({
+    storage,
+    limits:{
+        fileSize: 20 * 1024 * 1024
+    },
+    fileFilter
+})
+
+module.exports = upload
